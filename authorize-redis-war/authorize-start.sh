d1dff90afdb5699edadf7bed7d1e27ac1d06d838
2506225465@qq.com <2506225465@qq.com>
2019-08-25 20:14:24 +0800

    changed

--- authorize-redis-war/authorize-start.sh
+++ authorize-redis-war/authorize-start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #echo '192.168.0.118 www.mq.ingress' >> /etc/hosts
 #mv /jar/mq-adapter-boot-zuul.jar /app/
+echo 'ls -l /tomcat'
 ls -l /tomcat
+echo 'ls -l /tomcat/8'
+ls -l /tomcat/8
 /tomcat/8/bin/catalina.sh start
