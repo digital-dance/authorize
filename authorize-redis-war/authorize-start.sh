9b5b5a1674afd8eb9fc85b0fc0b695465309c227
2506225465@qq.com <2506225465@qq.com>
2019-08-25 21:27:31 +0800

    changed

--- authorize-redis-war/authorize-start.sh
+++ authorize-redis-war/authorize-start.sh
@@ -12,4 +12,13 @@ ls -l /tomcat/8/webapps
 echo 'ls -l /tomcat/8/webapps/ROOT'
 ls -l /tomcat/8/webapps/ROOT
 
+mkdir -p /app/logs
+mkdir -p /app/logs/authorize
+mkdir -p /app/logs/authorize/logs
+
+chmod -R 777 /app
+chmod -R 777 /app/*
+chmod -R 777 /app/logs/authorize
+chmod -R 777 /app/logs/authorize/logs
+
 /tomcat/8/bin/catalina.sh start
